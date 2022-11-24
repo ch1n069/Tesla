d72a7e4118cfb4f5d4294e6eb266f4f385704aef
Bruno Alpachino <brunolalpachino@gmail.com>
2022-11-24 12:46:32 +0300

    imported my section component into the home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Section from "../components/Section";
+import "./Home.css";
 
+// all imports happen above
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home__container">
+      Home
+      <Section />
+    </div>
+  );
 };
 
 export default Home;
