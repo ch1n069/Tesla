d696253b988ed37adfeab825d339ea887fc5cef6
Bruno Alpachino <brunolalpachino@gmail.com>
2022-11-24 12:46:57 +0300

    :paint: styling of the section component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div>
-      <Header />
+      <Home />
     </div>
   );
 }
